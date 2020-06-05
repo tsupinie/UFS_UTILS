56ea1eba3d48b6fc58ead05dc5e66ab9939e2b90
Tim Supinie <tsupinie@ou.edu>
2020-06-05 16:38:44 -0500

    Added parallel LBC processing to CAPS chgres scripts

--- ush/caps_chgres_cube.sh
+++ ush/caps_chgres_cube.sh
@@ -2,13 +2,13 @@
 #SBATCH -A TG-ATM160026
 #SBATCH -J chgres_cube
 #SBATCH -t 01:00:00
-#SBATCH -n 12
+#SBATCH -n 48
 #SBATCH -N 1
 #SBATCH -o /scratch/01479/tsupine/chgres_cube.out
 #SBATCH -e /scratch/01479/tsupine/chgres_cube.out
 #SBATCH -p skx-dev
 
-MEMBER=${MEMBER:-nam}
+MEMBER=${MEMBER:-gfs}
 
 export HOMEufs=/work/01479/tsupine/stampede2/software/UFS_UTILS
 export FIXfv3=${GRID_PATH:-/work/01479/tsupine/fv3-hmt-2020/grid}
@@ -16,7 +16,7 @@ cd $HOMEufs/ush
 
 export CRES=768
 
-export CDATE=${CDATE:-2020042900}
+export CDATE=${CDATE:-2020042800}
 CYCLE=`echo $CDATE | cut -c 9-10`
 
 export INPUT_TYPE='grib2'
@@ -41,7 +41,6 @@ export HALO_BLEND=8
 export VARMAP_FILE=$HOMEufs/parm/varmap_tables/GFSphys_var_map.txt
 export OROG_FILES_TARGET_GRID="C${CRES}_oro_data.tile7.halo4.nc"
 
-export APRUN='ibrun -n 12 -o 0'
 OUT_PATH=${OUT_PATH:-/scratch/01479/tsupine/chgres_cube/$CDATE}
 
 #if [ -d $OUT_PATH ]; then
@@ -49,10 +48,13 @@ OUT_PATH=${OUT_PATH:-/scratch/01479/tsupine/chgres_cube/$CDATE}
 #fi
 mkdir $OUT_PATH
 
-FCST_LEN=${FCST_LEN:-0}
+FCST_LEN=${FCST_LEN:-6}
+nfcst=$((FCST_LEN / LBC_DT))
+nprocs_per_fcst=24
 
-for ((ifhr_=0;ifhr_<=$FCST_LEN;ifhr_+=$LBC_DT)); do
-    ifhr=`printf "%02d" $ifhr_`
+for ((ifcst=0;ifcst<=$nfcst;ifcst++)); do
+    export APRUN="ibrun -n $nprocs_per_fcst -o $((ifcst * nprocs_per_fcst))"
+    ifhr=`printf "%02d" $((ifcst * LBC_DT))`
 
     export GRIB2_FILE_INPUT=$GRIB2_FILE_BASE$ifhr
 
@@ -65,7 +67,7 @@ for ((ifhr_=0;ifhr_<=$FCST_LEN;ifhr_+=$LBC_DT)); do
         export REGIONAL=2
     fi
 
-    ./chgres_cube.sh
+    (./chgres_cube.sh > $DATA/chgres_cube.out) &
 
     ln -s f$ifhr/gfs.bndy.nc $OUT_PATH/gfs_bndy.tile7.0$ifhr.nc
     if [ $ifhr == 00 ]; then
@@ -74,3 +76,5 @@ for ((ifhr_=0;ifhr_<=$FCST_LEN;ifhr_+=$LBC_DT)); do
         ln -s f${ifhr}/out.sfc.tile1.nc $OUT_PATH/sfc_data.nc
     fi
 done
+
+wait
